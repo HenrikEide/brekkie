79798d246712819f450eaaffa3d4d2aa65fe134c
HenrikEide <Henrik225@gmail.com>
2024-08-29 11:08:15 +0200

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
+} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
-import DeleteIcon from '@mui/icons-material/Delete';
+import DeleteIcon from "@mui/icons-material/Delete";
 import {
   Dialog,
   DialogContent,
@@ -16,13 +16,10 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogFooter,
-  DialogClose
-} from "@/components/ui/dialog"
-
-
+  DialogClose,
+} from "@/components/ui/dialog";
 
 export default function Home() {
-
   type Breakfast = {
     id: string;
     name: string;
@@ -35,87 +32,106 @@ export default function Home() {
   };
 
   const [breakfasts, setBreakfasts] = useState<Breakfast[]>([]);
-  
+
   useEffect(() => {
     fetch("http://localhost:5286/breakfasts")
       .then((response) => response.json())
       .then((data) => {
-      setBreakfasts(data);
+        setBreakfasts(data);
       })
       .catch((error) => {
-      console.error("Failed to fetch breakfasts:", error);
-      fetch("beekfast.json")
-        .then((response) => response.json())
-        .then((data) => {
-          setBreakfasts(data);
-        });
+        console.error("Failed to fetch breakfasts:", error);
+        fetch("beekfast.json")
+          .then((response) => response.json())
+          .then((data) => {
+            setBreakfasts(data);
+          });
       });
-  }
-  , []);
+  }, []);
 
   const deleteBreakfast = (id: string) => {
     fetch(`http://localhost:5286/breakfasts/${id}`, {
       method: "DELETE",
     });
     setBreakfasts(breakfasts.filter((b) => b.id !== id));
-  }
+  };
 
   return (
-      <>
-        {breakfasts.length === 0 && (
-          <p className="text-center">No breakfasts :(</p>
-        )}
-        <Accordion type="single" collapsible>
-          {breakfasts.map((breakfast) => (
-            <AccordionItem key={breakfast.id} value={breakfast.id} className="p-4 shadow rounded mb-8 bg-slate-800">
-              <AccordionTrigger>
-                <div >
-                  <h2>{breakfast.name}</h2>
-                </div>
-                <div className="ml-auto mr-2">
-                  <Dialog>
-                    <DialogTrigger asChild>
-                    <Button size="icon" variant="ghost" onClick={(e) => e.stopPropagation()}>
+    <>
+      {breakfasts.length === 0 && (
+        <p className="text-center">No breakfasts :(</p>
+      )}
+      <Accordion type="single" collapsible>
+        {breakfasts.map((breakfast) => (
+          <AccordionItem
+            key={breakfast.id}
+            value={breakfast.id}
+            className="p-4 shadow rounded mb-8 bg-slate-800"
+          >
+            <AccordionTrigger>
+              <div>
+                <h2>{breakfast.name}</h2>
+              </div>
+              <div className="ml-auto mr-2">
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      size="icon"
+                      variant="ghost"
+                      onClick={(e) => e.stopPropagation()}
+                    >
                       <DeleteIcon />
                     </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle className="text-black">Delete Breakfast</DialogTitle>
-                      </DialogHeader>
-                      <DialogDescription className="my-6">
-                        Are you sure you want to delete &quot;{breakfast.name}&quot;?
-                      </DialogDescription>
-                      <DialogFooter>
-                        <div className="flex justify-end space-x-4">
-                          <DialogClose asChild>
-                            <Button onClick={(e) => {
+                  </DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle className="text-black">
+                        Delete Breakfast
+                      </DialogTitle>
+                    </DialogHeader>
+                    <DialogDescription className="my-6">
+                      Are you sure you want to delete &quot;{breakfast.name}
+                      &quot;?
+                    </DialogDescription>
+                    <DialogFooter>
+                      <div className="flex justify-end space-x-4">
+                        <DialogClose asChild>
+                          <Button
+                            onClick={(e) => {
                               deleteBreakfast(breakfast.id);
-                              e.stopPropagation()}}>Delete
-                            </Button>
-                          </DialogClose>
-                          <DialogClose asChild>
-                            <Button variant="secondary" onClick={(e) => e.stopPropagation()}>Cancel</Button>
-                          </DialogClose> 
-                        </div>
-                      </DialogFooter>
-                    </DialogContent>
-                  </Dialog>
-                </div>
-              </AccordionTrigger>
-              <AccordionContent>
-                <div>
-                  <p>{breakfast.description}</p> <br/>
-                  <p>Savory: {breakfast.savory.join(", ")}</p>
-                  <p>Sweet: {breakfast.sweet.join(", ")}</p> <br/>
-                  <p>Start Time: {breakfast.startDateTime}</p>
-                  <p>End Time: {breakfast.endDateTime}</p>
-                  <p>Last Modified: {breakfast.lastModifiedDateTime}</p>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </Accordion>
-      </>
+                              e.stopPropagation();
+                            }}
+                          >
+                            Delete
+                          </Button>
+                        </DialogClose>
+                        <DialogClose asChild>
+                          <Button
+                            variant="secondary"
+                            onClick={(e) => e.stopPropagation()}
+                          >
+                            Cancel
+                          </Button>
+                        </DialogClose>
+                      </div>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>
+              <div>
+                <p>{breakfast.description}</p> <br />
+                <p>Savory: {breakfast.savory.join(", ")}</p>
+                <p>Sweet: {breakfast.sweet.join(", ")}</p> <br />
+                <p>Start Time: {breakfast.startDateTime}</p>
+                <p>End Time: {breakfast.endDateTime}</p>
+                <p>Last Modified: {breakfast.lastModifiedDateTime}</p>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
   );
 }
